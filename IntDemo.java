553f74a512d200697a237a02528955e5b9cabb59
lakshay-sharma-2024 <158801088+lakshay-sharma-2024@users.noreply.github.com>
2024-02-16 22:57:31 +0530

    Add files via upload

---
+++ IntDemo.java
@@ -0,0 +1,10 @@
+package Assignment6;
+
+public class IntDemo {
+        public static void main(String[] k) {
+
+            InterfaceImplementation i = new InterfaceImplementation();
+            i.method1();
+            i.method1();
+        }
+    }
---
+++ Interface1.java
@@ -0,0 +1,10 @@
+package Assignment6;
+
+public interface Interface1 {
+        int a=2;
+        public void method1();
+        public void method2();
+    }
+
+
+
